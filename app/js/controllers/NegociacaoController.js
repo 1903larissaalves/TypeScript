c15ef804146bd311bc810b22b670dc1bb7eb8272
Larissa Alves <1903larissaalves@gmail.com>
2020-12-21 08:48:56 -0300

    Adicionado controller

---
+++ app/js/controllers/NegociacaoController.js
@@ -0,0 +1,12 @@
+class NegociacaoController {
+    constructor() {
+        this._data = document.querySelector("#data");
+        this._quantidade = document.querySelector("#quantidade");
+        this._valor = document.querySelector("#valor");
+    }
+    adiciona(event) {
+        event.preventDefault();
+        const negociacao = new Negociacao(this._data.value, this._quantidade.value, this._valor.value);
+        console.log(negociacao);
+    }
+}
---
+++ app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,26 @@
+class NegociacaoController{
+
+    private _data;
+    private _quantidade;
+    private _valor;
+
+    constructor(){
+        this._data = document.querySelector("#data");
+        this._quantidade = document.querySelector("#quantidade");
+        this._valor = document.querySelector("#valor");
+    }
+
+    adiciona(event){
+
+        event.preventDefault();
+
+        const negociacao = new Negociacao(
+            this._data.value,
+            this._quantidade.value,
+            this._valor.value
+        );
+
+        console.log(negociacao);
+        
+    }
+}
